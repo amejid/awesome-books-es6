84f9f0108f0b3c2f44e4fd3ea2ce0d40404f824d
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-03 13:20:24 +0300

    Implement page navigation

---
+++ modules/views/navigationView.js
@@ -0,0 +1,30 @@
+class NavigationView {
+  #navLinksEl = document.querySelector('.nav-links');
+
+  #navLinkEl = document.querySelectorAll('.nav-link');
+
+  #sectionEl = document.querySelectorAll('.section');
+
+  #yearEl = document.querySelector('.year');
+
+  setYear(time) {
+    this.#yearEl.textContent = time;
+  }
+
+  navigate() {
+    this.#navLinksEl.addEventListener('click', (e) => {
+      const clicked = e.target;
+      if (!clicked.classList.contains('nav-link')) return;
+
+      const { sect } = clicked.dataset;
+      this.#navLinkEl.forEach((link) => link.classList.remove('nav-link--active'));
+      clicked.classList.add('nav-link--active');
+
+      this.#sectionEl.forEach((link) => link.classList.remove('section--active'));
+
+      document.querySelector(`.section-${sect}`).classList.add('section--active');
+    });
+  }
+}
+
+export default new NavigationView();
